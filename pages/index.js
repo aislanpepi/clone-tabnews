2f9e43d05a44fb1a401c6bf10079ad530ae76126
Aislan <aislan.pepi@outlook.com>
2025-12-26 14:46:10 -0300

    refactor: modificado a frase novamente

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Quando surge o alviverde imponente</h1>
+    return <h1>Quando surge o alviverde imponente. No gramado em que a luta o aguarda.</h1>
 }
 
 export default Home;
