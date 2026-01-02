f7a0af35dd05eae2ff819f59183a0b7d5585a66f
Aislan <aislan.pepi@outlook.com>
2026-01-02 17:44:42 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home(){
-    return <h1>Quando surge o alviverde imponente. No gramado em que a luta o aguarda.</h1>
+function Home() {
+  return (
+    <h1>
+      Quando surge o alviverde imponente. No gramado em que a luta o aguarda.
+    </h1>
+  );
 }
 
 export default Home;
